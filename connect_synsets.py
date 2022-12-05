6eba230c26d0acb2484ce14aec9555401e790fd0
Rishi Upadhyay <rishiu@ucla.edu>
2022-12-05 05:00:25 +0000

    add num to class dict

--- connect_synsets.py
+++ connect_synsets.py
@@ -1,24 +1,45 @@
 import os
 import sys
+import json
+
+def file_to_file(fname, out_fname):
+    map_dict = {}
+    with open(fname, "r") as map_file:
+        for line in map_file:
+             num, cls_num, name = line.split(" ")
+             map_dict[num] = (cls_num.strip(), name.strip())
+    with open(out_fname, "w") as out_file:
+        json.dump(map_dict, out_file)
+
+def file_to_dist(fname):
+    with open(fname, "r") as dict_file:
+        data = json.load(dict_file)
+        return data
 
 if __name__ == "__main__":
+    file_to_file("map_clsloc.txt", "clsloc_dict.txt")
+    exit(0)
+
     if len(sys.argv) < 4:
         exit("Need paths to SD dataset, ImageNet, and output file")
     sd_dataset = sys.argv[1]
     imagenet = sys.argv[2]
-    output_file = sys.argv[3]
+    num_to_class_file = sys.argv[3]
+    output_file = sys.argv[4]
+    
+    num_to_class = file_to_dist(num_to_class_file)
 
     sd_nums = []
     for file in os.listdir(sd_dataset):
         if file.find("txt") >= 0:
-            with open(file, "r") as word_file:
-                wordnet_num = int(word_file.readline())
+            with open(sd_dataset+file, "r") as word_file:
+                wordnet_num = int(word_file.readline().strip()[1:])
                 sd_nums.append(wordnet_num)
 
     in_datadir = imagenet + "Data/CLS-LOC/"
 
     common_nums = []
-    for phase in ["train/","test/","valid/"]:
+    for phase in ["train/"]:
         data_dir = in_datadir + phase
 
         for file in os.listdir(data_dir):
@@ -26,6 +47,6 @@ if __name__ == "__main__":
                 if file.find(str(num)) >= 0:
                     common_nums.append(num)
     with open(output_file, "w") as out_file:
-        joined = '\n'.join(common_nums)
+        joined = '\n'.join(map(str, common_nums))
         out_file.write(joined)
 
