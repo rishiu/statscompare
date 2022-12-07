b13b2e7d98302c67617c8c381737ee8aaf32d4ac
Rishi Upadhyay <rishiu@ucla.edu>
2022-12-06 17:37:07 -0800

    add function to get name dict

--- utils.py
+++ utils.py
@@ -12,6 +12,15 @@ def file_to_file(fname, out_fname):
     with open(out_fname, "w") as out_file:
         json.dump(map_dict, out_file)
 
+def file_to_file2(fname, out_fname):
+    map_dict = {}
+    with open(fname, "r") as map_file:
+        for line in map_file:
+             num, cls_num, name = line.split(" ")
+             map_dict[name] = (cls_num.strip(), num.strip())
+    with open(out_fname, "w") as out_file:
+        json.dump(map_dict, out_file)
+
 def file_to_dict(fname):
     with open(fname, "r") as dict_file:
         data = json.load(dict_file)
