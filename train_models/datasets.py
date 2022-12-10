852d7269ff0dfaf0885e8ecd1d6f0a57f27cbcea
Rishi Upadhyay <rishiu@ucla.edu>
2022-12-10 01:43:47 +0000

    get dataloading working for synthetic and real images

--- train_models/datasets.py
+++ train_models/datasets.py
@@ -2,55 +2,64 @@ import numpy as np
 import torch
 from torch import nn
 from torch.utils.data import Dataset
-from torchvision.transforms import ToTensor
+from torchvision import transforms
 from torchvision.io import read_image
 from torchvision.models import efficientnet_b4
 import glob
 import sys
 import os
 sys.path.append("../")
-from utils import file_to_dict, get_common_synsets
+from utils import file_to_dict, get_imagenet100_synsets
 
-class SyntheticImageNetDataset(Dataset):
-    def __init__(self,data_dir,fname):
-        self.data_dir = data_dir
-        self.map_dict = file_to_dict(fname)
-
-    def __len__(self):
-        return len(glob.glob("*.jpg"))
-
-    def __getitem__(self, idx):
-        img_files = glob.glob("*.jpg")
-        file = img_files[idx]
-
-        name = file[:-4]
-        class_num = int(self.map_dict[name][0])
-
-        label = np.zeros(1000)
-        label[class_num] = 1.0
-
-        img = read_image(self.data_dir+file)
-        img = img[:3,:,:]
-
-        return img, label
-
-def dataset_helper(data_dir, fname, map_dict):
-    synsets = get_common_synsets(fname)
+def syn_dataset_helper(data_dir, fname, map_dict):
+    synsets = get_imagenet100_synsets(fname)
 
     items = []
-    for synset in synsets:
-        class_num = map_dict[synset]
-        files = os.listdir(data_dir+"n0"+str(synset)+"/")
-        idx = np.random.randint(0,len(files),size=10)
+    for index, synset in enumerate(synsets):
+        syn_id = "n{:08d}".format(synset)
+        syn_info = map_dict[syn_id]
+        class_num = int(syn_info[0])
+        class_name = syn_info[1].strip().lower()
+        
+        syn_id = "{:08d}".format(synset)
+        base_dir = data_dir+class_name+"/"
+        files = os.listdir(base_dir)
+        idx = np.arange(30)
+        np.random.shuffle(idx)
         for i in idx:
-            items.append((files[i], class_num))
+            items.append((base_dir+files[i], index))
     return items
 
-class RealImageNetDataset(Dataset):
-    def __init__(self,data_dir,fname):
+def real_dataset_helper(data_dir, fname, map_dict):
+    synsets = get_imagenet100_synsets(fname)
+
+    items = []
+    for index, synset in enumerate(synsets):
+        syn_id = "n{:08d}".format(synset)
+        class_num = int(map_dict[syn_id][0])
+        base_dir = data_dir+syn_id+"/"
+        files = os.listdir(base_dir)
+        idx = np.arange(40)
+        np.random.shuffle(idx)
+        for i in idx:
+            items.append((base_dir+files[i], index))
+    return items
+
+class SyntheticImageNetDataset(Dataset):
+    def __init__(self,data_dir,map_fname,syn_fname):
         self.data_dir = data_dir
-        self.map_dict = file_to_dict(fname)
-        self.items = dataset_helper(data_dir,fname,self.map_dict)
+        self.map_dict = file_to_dict(map_fname)
+        self.items = syn_dataset_helper(data_dir,syn_fname,self.map_dict)
+        self.transform = transforms.Compose([
+        	transforms.Resize(256),
+        	transforms.RandomHorizontalFlip(0.5),
+        	transforms.RandomCrop(224),
+        	transforms.Lambda(self.normalize)])
+    
+    def normalize(self, x):
+    	x /- 127.5
+    	x -= 1
+    	return x 	
 
     def __len__(self):
         return len(self.items)
@@ -58,10 +67,47 @@ class RealImageNetDataset(Dataset):
     def __getitem__(self, idx):
         fpath, cls = self.items[idx]
 
-        img = read_image(fpath)[:3,:,:]
+        
+        img = read_image(fpath).float()
+        
+        if img.shape[0] == 1:
+            img = img.repeat(3,1,1)
+        
+        img = self.transform(img)
 
-        label = np.zeros(1000)
-        label[cls] = 1.0
+        return img, cls
 
-        return img, label
+class RealImageNetDataset(Dataset):
+    def __init__(self,data_dir,map_fname, syn_fname):
+        self.data_dir = data_dir
+        self.map_dict = file_to_dict(map_fname)
+        self.items = real_dataset_helper(data_dir,syn_fname,self.map_dict)
+        self.transform = transforms.Compose([
+            transforms.Resize(256),
+            transforms.RandomHorizontalFlip(0.5),
+            transforms.RandomCrop(224),
+            transforms.Lambda(self.normalize)])
+        	
+    def normalize(self, x):
+        x /= 127.5
+        x -= 1
+        return x
+
+    def __len__(self):
+        return len(self.items)
+
+    def __getitem__(self, idx):
+        fpath, cls = self.items[idx]
+
+        img = read_image(fpath).float()
+        
+        if img.shape[0] == 1:
+            img = img.repeat(3,1,1)
+        
+        img = self.transform(img)
+
+        #label = np.zeros(100)
+        #label[cls] = 1.0
+
+        return img, cls
 
--- train_models/models.py
+++ train_models/models.py
@@ -5,9 +5,12 @@ from torch.utils.data import DataLoader
 from torchvision.models import efficientnet_b4
 import torch.optim as optim
 import os
+import sys
 from tqdm import tqdm
 import pytorch_lightning as pl
 from datasets import SyntheticImageNetDataset, RealImageNetDataset
+sys.path.append("../")
+from utils import file_to_file2
 
 class ImageNetEfficientNet(pl.LightningModule):
     def __init__(self):
@@ -17,12 +20,12 @@ class ImageNetEfficientNet(pl.LightningModule):
     def training_step(self, batch, batch_idx):
         x, y = batch
         z = self.model(x)
-
-        loss = F.cross_entropy(y,z)
+	
+        loss = F.cross_entropy(z,y)
         return loss
 
     def configure_optimizers(self):
-        optimizer = optim.Adam(self.parameters(), lr=1e-3)
+        optimizer = optim.Adam(self.parameters(), lr=0.0005)
         return optimizer
 
     def validation_step(self, batch, batch_idx):
@@ -44,12 +47,16 @@ def train(epochs, train_dir, val_dir, flag="S", save_dir="./"):
 
     model = ImageNetEfficientNet()
     
-    train_dataset = dataset(train_dir)
-    val_dataset = dataset(val_dir)
+    train_dataset = dataset(train_dir, "../clsloc_dict.txt", "../imagenet20_classes.txt")
+    #val_dataset = dataset(val_dir)
 
-    train_dataloader = DataLoader(train_dataset)
-    val_dataloader = DataLoader(val_dataset)
+    train_dataloader = DataLoader(train_dataset, shuffle=True, batch_size=1, num_workers=4)
+    #val_dataloader = DataLoader(val_dataset)
 
-    trainer = pl.Trainer(accelerator='gpu', devices=[0], max_epochs=epochs, default_root_dir=save_dir)
-    trainer.fit(model, train_dataloader, val_dataloader)
+    trainer = pl.Trainer(accelerator='gpu', devices=[0,1], max_epochs=epochs, default_root_dir=save_dir)
+    trainer.fit(model, train_dataloader)
+    
+if __name__ == "__main__":
+    #file_to_file2("../map_clsloc.txt", "../nameloc_dict.txt")
+    train(100, sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
 
